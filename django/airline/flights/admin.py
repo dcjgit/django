09bae798369f86af3f7afe8d838240941357a6fe
Dinesh Jesudas <dinesh.jesudas@gmail.com>
2020-08-06 08:41:48 -0400

    Added admin customization for flights and passengers

--- django/airline/flights/admin.py
+++ django/airline/flights/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from .models import Flight, Airport, Passenger
 
 # Register your models here.
+
+## We can customize the admin feature... with what columns we want to display.
+## Here, we want to see more info about the flights...
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights", )
+
 admin.site.register(Airport)
-admin.site.register(Flight)
-admin.site.register(Passenger)
+# Register the flight with FlightAdming settings...
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
